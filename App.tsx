2280950174f69ecbfd6e04bffa83ce8c3f4b8c1d
Matheus Andrade <matheusandrade.ma2003@gmail.com>
2023-04-09 00:21:52 -0300

    Expo Prebuild

--- App.tsx
+++ App.tsx
@@ -1,5 +1,7 @@
 import { StatusBar, View } from "react-native";
 
+import OneSignal from 'react-native-onesignal';
+
 import { NativeBaseProvider } from "native-base";
 
 import {
@@ -15,6 +17,8 @@ import { THEME } from "./src/theme";
 import { Loading } from "@components/Loading";
 
 import { Routes } from "@routes/index";
+
+OneSignal.setAppId("deab7f33-7872-406c-8f8d-9668d0c4b8c7");
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
